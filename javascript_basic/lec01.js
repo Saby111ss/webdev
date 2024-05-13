48d666cd2d096c331526574648c8dfa9b68b89aa
Saby111ss <156421242+Saby111ss@users.noreply.github.com>
2024-05-13 09:02:23 +0000

    js div

---
+++ javascript_basic/lec01.js
@@ -0,0 +1,23 @@
+// * word vs keyword
+
+//Saby ->> word
+//is   ->> word
+//good ->> word
+//man  ->> word
+//for  ->> keyword
+//if   ->> keyword
+//else ->> keyword
+//var  ->> keyword
+//let  ->> keyword
+
+//* var const let
+
+//var is variable
+//const = constant
+//let = variable
+
+// *hosting = variable & functions are hoisted which means their declaration is moved on the top of code
+// variable banne se pehle use use kar sakte hain
+console.log(a);
+var a = 12;
+ 
